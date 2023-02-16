13e60cf3c46a80e930dfe1a7dbce238b7931d55c
TurtleY0da <90482934+TurtleY0da@users.noreply.github.com>
2023-02-16 11:21:50 -0700

    Finished Assignement

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 //#region Initialize Variables
 let firstNameEl = document.getElementById("firstNameIn");
-let lastNameEl = document.getElementById("firstNameIn");
+let lastNameEl = document.getElementById("lastNameIn");
 
 let modeSelectEl = document.getElementById("modeSelect");
 let goBtnEl = document.getElementById("goBtn");
@@ -12,7 +12,7 @@ let outputEl = document.getElementById("output");
 let nicknames = ["Boomer", "Ace", "Captain Obvious", "Goofball"];
 //#endregion
 
-goBtnEl.addEventListener("mousedown", getOption);
+goBtnEl.addEventListener("click", getOption);
 
 //#region Functions
 function writeOut(output) {
@@ -28,12 +28,63 @@ function writeOut(output) {
 
 function getOption() {
   switch (modeSelectEl.value) {
+    case "displayAll":
+      writeOut(displayAll());
+      break;
+    case "displayRandom":
+      writeOut(displayRandom());
+      break;
+    case "addNick":
+      writeOut(addNick());
+      break;
+    case "removeLast":
+      writeOut(removeIndex(nicknames.length - 1));
+      break;
+    case "removeIndex":
+      writeOut(removeIndex(prompt("Index to remove:")));
+      break;
+    case "removeName":
+      writeOut(removeName());
+      break;
     default:
     case "none":
-      writeOut('Select a mode and press "Go"');
-      break;
-    case "displayAll":
+      writeOut('Select a Mode and Press "Go"');
       break;
   }
+}
+
+function displayAll() {
+  let result = new Array();
+  for (let i = 0; i < nicknames.length; i++) {
+    result.push(`${firstNameEl.value} "${nicknames[i]}" ${lastNameEl.value}`);
+  }
+  return result;
+}
+
+function displayRandom() {
+  let randInt = Math.floor(Math.random() * nicknames.length);
+  let result = `${randInt}: ${firstNameEl.value} "${nicknames[randInt]}" ${lastNameEl.value}`;
+  return result;
+}
+
+function addNick() {
+  let newNick = prompt("Name to add:");
+  if (nicknames.includes(newNick))
+    return `Nickname "${newNick}" Already Exists`;
+  nicknames.push(newNick);
+  return `Nickname Added: ${newNick}`;
+}
+
+function removeIndex(index) {
+  if (index < 0 || index >= nicknames.length) return "Invalid Index";
+  let removedNick = nicknames.splice(index, 1);
+  return `Removed Nickname at Index ${index}: ${removedNick[0]}`;
+}
+
+function removeName() {
+  let removeNick = prompt("Name to remove:");
+  let removeIndex = nicknames.indexOf(removeNick);
+  if (removeIndex === -1) return "Invalid Name";
+  return removeIndex(removeIndex);
 }
 //#endregion
